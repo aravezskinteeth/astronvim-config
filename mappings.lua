09d946cb51f92a582adeeefb9cecac9b05714864
aravezskinteeth <kelthompson@gmail.com>
2023-04-12 18:01:07 -0500

    update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -21,7 +23,7 @@ return {
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
 
-    ["<leader>a"] = { "<cmd>echo 'Hello world!'<cr>", desc = "Say hello world" },
+    -- ["<leader>a"] = { "<cmd>echo 'Hello world!'<cr>", desc = "Say hello world" },
   },
   t = {
     -- setting a mapping to false will disable it
