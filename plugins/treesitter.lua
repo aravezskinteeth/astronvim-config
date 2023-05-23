007dac1793d8472401927cb93f726a4e32a60630
aravezskinteeth <kelthompson069@gmail.com>
2023-05-23 18:36:57 -0500

    i have no idea what im doing help

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -47,59 +47,67 @@ return {
         -- "vue",
         "yaml",
       },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+        -- optional (with quarto-vim extension and pandoc-syntax)
+        -- additional_vim_regex_highlighting = { 'markdown' },
 
-       -- note: the vim regex based highlighting from
-       -- quarto-vim / vim-pandoc sets the wrong comment character
-       -- for some sections where there is `$` math.
-    },
-    indent = {
-      enable = true,
-    },
-    incremental_selectio = {
-      enable = true,
-      keymaps = {
-        init_selection = "gnn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
+        -- note: the vim regex based highlighting from
+        -- quarto-vim / vim-pandoc sets the wrong comment character
+        -- for some sections where there is `$` math.
       },
-    },
-    textobjects = {
-      select = {
+      indent = {
         enable = true,
-        lookahead = true,
+      },
+      incremental_selection = {
+        enable = true,
         keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
-          ['ao'] = '@codechunk.outer',
-          ['io'] = '@codechunk.inner',
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
         },
       },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']m'] = '@function.outer',
-          [']c'] = '@codechunk.inner',
-          [']]'] = '@class.outer',
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
+            ['ao'] = '@codechunk.outer',
+            ['io'] = '@codechunk.inner',
+          },
         },
-        goto_next_end = {
-          [']M'] = '@function.outer',
-          [']['] = '@class.outer',
-        },
-        goto_previous_start = {
-          ['[m'] = '@function.outer',
-          ['[c'] = '@codechunk.inner',
-          ['[['] = '@class.outer',
-        },
-         goto_previous_end = {
-          ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            [']m'] = '@function.outer',
+            [']c'] = '@codechunk.inner',
+            [']]'] = '@class.outer',
+          },
+          goto_next_end = {
+            [']M'] = '@function.outer',
+            [']['] = '@class.outer',
+          },
+          goto_previous_start = {
+            ['[m'] = '@function.outer',
+            ['[c'] = '@codechunk.inner',
+            ['[['] = '@class.outer',
+          },
+          goto_previous_end = {
+            ['[M'] = '@function.outer',
+            ['[]'] = '@class.outer',
+          },
         },
       },
     },
-  }
+  },
+  { 'nvim-treesitter/nvim-treesitter-textobjects' },
 }
+
