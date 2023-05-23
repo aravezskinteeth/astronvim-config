2051e07176a881e0980274bc9fde4527f2dd6e68
aravezskinteeth <kelthompson069@gmail.com>
2023-05-23 12:37:02 -0500

    rewrite general snippet

--- snippets/all.lua
+++ snippets/all.lua
@@ -4,23 +4,9 @@
 -- ~~~~~ Abbreviations used in the LuaSnip docs ~~~~~ --
 local ls = require("luasnip")
 local s = ls.snippet
--- local sn = ls.snippet_node
 local t = ls.text_node
--- local i = ls.insert_node
--- local f = ls.function_node
--- local d= ls.dynamic_node
--- local fmt = require("luasnip.extras.fmt").fmt
--- local fmta = require("luasnip.extras.fmt").fmta
--- local rep = require("luasnip.extras").rep
 
--- ~~~~~ The actual hello world snippet ~~~~~ --
+
 return {
--- A simple "Hello, world!" text node
-s(
-  {trig = "hi"}, -- Table of snippet parameters
-  { -- Table of snippet nodes
-    t("Hello, world!")
-  }
-),
+  s({ trig = "trig", snippetType = "autosnippet" }, t "loaded!!"),
 }
-
