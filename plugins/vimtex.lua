93fecf3e7bfc953a674f412fe41dcdbe6a0de5c8
aravezskinteeth <kelthompson069@gmail.com>
2023-05-24 11:15:57 -0500

    add keymaps

--- plugins/vimtex.lua
+++ plugins/vimtex.lua
@@ -2,10 +2,24 @@ return {
   -- VImTex configuration
   {
     'lervag/vimtex',
-      init = function ()
-        vim.g.vimtex_view_method = "zathura"
-      end,
-      event = "BufRead",
+    init = function ()
+      vim.g.vimtex_view_method = "zathura"
+    end,
+    keys = {
+      {
+        "<leader>Vh", "<cmd>:help vimtex-default-mappings<cr>", desc = "VimTex Help"
+      },
+      {
+        "<leader>Vo", "<cmd>:VimtexTocOpen<cr>", desc = "VimTex: Open Table of Contents"
+      },
+      {
+        "<leader>Vt", "<cmd>:VimtexTocToggle<cr>", desc = "VimTex: Toggle Table of Contents"
+      },
+      {
+        "<leader>Vc", "<cmd>:VimtexCompile<cr>", desc = "VimTex: Compile PDF"
+      },
+    },
+    event = "BufRead",
   },
 }
 
