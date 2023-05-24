b550b229d5714e0f704ea9c313bc102c7817b6ce
aravezskinteeth <kelthompson069@gmail.com>
2023-05-24 11:15:20 -0500

    add keymaps

--- plugins/glow.lua
+++ plugins/glow.lua
@@ -4,6 +4,14 @@ return {
     "ellisonleao/glow.nvim",
     config = true,
     cmd = "Glow",
+    keys = {
+      {
+        "<leader>Go", "<cmd>:Glow<cr>", desc = "Preview File"
+      },
+      {
+        "<leader>Gc", "<cmd>:Glow!<cr>", desc = "Close Preview"
+      }
+    },
     -- Load plugin every time you open a file
     event = "User AstroFile",
   },
