53acc8da08b16997550043ee43eac98115b8d76f
skinofmyeden <apple@livisu.local>
2023-10-01 16:16:10 -0500

    Delete glow setup

--- plugins/glow.lua
+++ plugins/glow.lua
@@ -14,13 +14,5 @@ return {
     },
     -- Load plugin every time you open a file
     event = "User AstroFile",
-    require('glow').setup({
-      border = "shadow", -- floating window border config
-      pager = false,
-      width = 80,
-      height = 100,
-      width_ratio = 0.7, -- maximum width of the Glow window compared to the nvim window size (overrides `width`)
-      height_ratio = 0.7,
-    })
-  },
+   },
 }
