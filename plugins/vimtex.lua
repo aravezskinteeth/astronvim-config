fa967d1be35464fc889514a6a29829c7fdef448e
skinofmyeden <apple@livisu.local>
2024-01-12 19:35:09 -0500

    Add texflavor & quickfix settings

--- plugins/vimtex.lua
+++ plugins/vimtex.lua
@@ -3,7 +3,9 @@ return {
   {
     'lervag/vimtex',
     init = function ()
+      vim.g.tex_flavor = 'latex'
       vim.g.vimtex_view_method = 'sioyek'
+      vim.g.vimtex_quickfix_mode = 0
     end,
     keys = {
       {
